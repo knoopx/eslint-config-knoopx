79572deb9ac46382959d8ff2aa4e82b1e1441c4d
Victor Martinez <knoopx@gmail.com>
2019-08-22 13:12:01 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,19 @@
+# linter-eslint
+
+```
+git clone https://github.com/knoopx/eslint-config-knoopx ~/.eslint-config
+cd ~/.eslint-config
+yarn
+```
+
+```
+"linter-eslint":
+  autofix:
+    fixOnSave: true
+  disabling:
+    disableWhenNoEslintConfig: false
+  global:
+    eslintrcPath: "~/.eslint-config/index.js"
+    globalNodePath: "~/.eslint-config"
+    useGlobalEslint: true
+```
---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require("fs")
+const path = require("path")
+
+if (process.release.sourceUrl.includes("atom.io")) {
+  process.env.NODE_PATH = path.resolve(__dirname, "node_modules")
+  require("module").Module._initPaths()
+}
+
+module.exports = {
+  extends: [
+    "eslint-config-airbnb-base",
+    "eslint-config-airbnb-base/rules/strict",
+    "eslint-config-airbnb/rules/react",
+    "plugin:prettier/recommended",
+    "prettier/babel",
+    "prettier/react",
+    "plugin:jsx-classname/recommended",
+    "plugin:jsx-classname/tailwindcss",
+  ],
+  rules: {
+    "babel/camelcase": "error",
+    "babel/new-cap": "error",
+    "babel/no-invalid-this": "error",
+    "babel/no-unused-expressions": "error",
+    "babel/valid-typeof": "error",
+    "camelcase": "off",
+    "class-methods-use-this": "off",
+    "func-names": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+    "new-cap": "off",
+    "no-alert": "off",
+    "no-invalid-this": "off",
+    "no-restricted-globals": "off",
+    "no-unused-expressions": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "all",
+        tabWidth: 2,
+        semi: false,
+        singleQuote: false,
+        quoteProps: "consistent",
+        arrowParens: "always",
+      },
+    ],
+    "react/forbid-prop-types": "off",
+    "react/no-unescaped-entities": "off",
+    "react/react-in-jsx-scope": "off",
+    "valid-typeof": "off",
+  },
+  plugins: ["import", "prettier", "babel", "react", "jsx-classname"],
+  settings: {},
+}
+
+const webpackConfig = path.resolve("webpack.config.js")
+
+if (fs.existsSync(webpackConfig)) {
+  Object.assign(module.exports.settings, {
+    "import/resolver": {
+      webpack: {
+        config: webpackConfig,
+      },
+    },
+  })
+}
